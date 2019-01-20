99e6c514ca480c187deb4f9e1d05afdeb120cb07
rotklocc <xellosgame@gmail.com>
2019-01-20 22:32:51 +0700

    add terrain advantage page

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,16 @@
+function createTextNode(tag, txt) {
+	var td = document.createElement(tag);
+	var txt = document.createTextNode(txt);
+	td.appendChild(txt);
+	return td;
+}
+
+function createHtmlNode(tag, html) {
+	var td = document.createElement(tag);
+	td.innerHTML = html;
+	return td;
+}
+
 function createTdTextNode(txt) {
 	var td = document.createElement("td");
 	var txt = document.createTextNode(txt);
@@ -43,3 +56,48 @@ function getCookie(cname) {
 	return "";
 }
 
+function dynamicCompareSortTable(table, colIdx) {
+	var sortedCol = -1;
+	var sortedAsc = (table.getAttribute('defaultSortAsc') === "1");
+	
+	if ('sortedCol' in table) {
+		sortedCol = table['sortedCol'];
+	}
+
+	if (sortedCol === colIdx) {
+		// Note: can do reverse instead of sorting (should be faster for large table)
+		// Note: 'sortedAsc' propery is always existed because 'sortedCol' must be existed to get in this case
+		sortedAsc = !table['sortedAsc'];
+	}
+	sortedCol = colIdx;
+	
+	// save current sort order to table object
+	table['sortedCol'] = sortedCol;
+	table['sortedAsc'] = sortedAsc;
+
+	return function (a, b) {
+		var aval = a.cells[sortedCol].svalue;
+		var bval = b.cells[sortedCol].svalue;
+		var ret = (aval < bval) ? -1 : (aval > bval) ? 1 : 0;
+		return sortedAsc ? ret : -ret;
+	}
+}
+
+function sortTableRows(table, colIdx) {
+	var rows = table.rows;
+	var rowParent = rows[0].parentNode;
+	var nrow = rows.length;
+	
+	// copy all rows to array
+	var rowArr = new Array(nrow);
+	for (var i = 0; i < nrow; i++) {
+		rowArr[i] = rows[i];
+	}
+	// sort
+	rowArr.sort(dynamicCompareSortTable(table, colIdx));
+	// re-add all rows back (much faster than using parentNode.insertBefore() for swapping rows)
+	rowParent.innerHTML = "";
+	for (var i = 0; i < nrow; i++) {
+		rowParent.appendChild(rowArr[i]);
+	}
+}
