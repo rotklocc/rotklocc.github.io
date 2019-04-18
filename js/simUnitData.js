4c47f504910604e19726da0a64b7581156f969bf
rotklocc <xellosgame@gmail.com>
2019-04-18 16:03:44 +0700

    more unit preset data

--- js/simUnitData.js
+++ js/simUnitData.js
@@ -2,10 +2,34 @@
 var unitPreset = {
 	// cao cao
 	1100001: 'mgsKWQRcAcAsRosaWRacAcAsBlmyo5GWwBwCwUl7Okxse0o8DEx8DEwFwMTBfAxLWgJMnAJMAkyEtqnJs',
+	// xu chu
+	1100016: 'mgWwpZGBwBwCxGixoZlhwBwCwGWbKjkgXAHALBSXs6XGxLSjwMTAHAxMBcDEwXwMS1oCTGPAJMAkwQtqnJs',
 	// sima yi
-	1100020: 'mhmwoJkADAHAXAE2saCJNcAcBcBlmyo5YnwBwFwUFbOlxsm0olxMTDnExMM8TExFxMS1qcAkwCTITAJLapyb',
+	1100020: 'mhmwoJkADAHAXAE2saCJNcAcBcBlmyo5YnwBwFwUFbOlxsm0olxMTDnExMM8TExFxMS1qcAkwCTITAJLapwb',
 	// xun yu
-	1100021: 'mhCwpZGHwBwFwBNrGlkpDAHAXAZZsqOWM8AcBcFBWzpcPHtKJcLEw5wsTDPCxMRcLEtanAKcApwAnAKbapyb',
+	1100021: 'mhCwpZGHwBwFwBNrGlkpDAHAXAZZsqOWM8AcBcFBWzpcPHtKJcLEw5wsTDPCxMRcLEtanAKcApwAnAKbapwb',
+	// pang tong
+	1100664: 'mlU7CgmQCcAcBcATaxpZKQwBwFwGWbKjlibAHAXBgVs6XBx7SjnCxMN8LExBwsTEXCxLWpwCnAKcAJwCm2qcG-',
+	// zhu huan
+	1100430: 'mjKbClkBTAHALEaLGlkUTAHALAZZsqORlsAcAsFxezpcbHtKPBxMfBxMBcHEwXwcS1oBTJwBTAFMAUtqDAs',
+	// sun shangxiang
+	1100478: 'mjZ7CgGSLAHALEaLGlkWnAHALAZZsqOSBcAcAsFJezpcbEtKPBxMfBxMBcHEwXwcS1oCTDHAJMAkxCtqDAs',
+	// ma yunlu
+	1100484: 'mjc7ClmYbAHALEaLGlkWnAHALAZZsqOSBcAcAsFJezo8bHtKPBxMfBxMBcHEwXwcS1oAnFnACcAJwAm2osm-',
+	// cai yan
+	1100475: 'mjZLClkYfAHAXAE2saWSkMAcBcBlmyo5YzwBwFwUFbOlxse0olwsTDnCxMM8LExFwsS1qcAZwBnAGcAZtqnBs',
+	// zgl (demon)
+	1100171: 'mgYLCgmQAMAcBcATaxoIk0wBwFwGWbKjlifAHAXBV1s6XJwbSiXCxMOcLEwzwsTEXCxLWpwDnAOcecA5tqnBs',
+	// young xiahou dun
+	1101045: 'mgk0sKWQWcAcRsRosaWROMAcRsBlmyo5lJwBxGwUl7Olxse0o8TEwBxMTAXExMF8TEtaAkw1wCTAJMOLapyb',
+	// fan kuai
+	1100852: 'mnQbCgmVbAHALEaLGhmWHAHALAZZsqORlsAcAsFJezpcTHtKAcLEwFwsTBPCxMF8LEtaA0ycA0wDTFS2qcm-',
+	// xiahou ying
+	1100853: 'mnQrClkBTAHALEaLGlkWnAHALAZZsqOUIcAcAsFVezpcTHtKPBxMAcHEwFwcTBfBxLWgJMnAJMAkyEtqDAs',
+	// zhongli mo
+	1101074: 'mgljsKWRhMAcAsRosaWRacAcAsBlmyo5QhwBwCwWN7OlxsS0o8HEwBwcTAXBxMF8HEtaA0ycA0wDTFS2oMC-',
 	// xiang yu
 	1100867: 'mnVrCgCWPAHALEaLGlkWnAHALAZZsqOUGMAcAsFVezpcbEtKPAxMfAxMGcDEwTwMS1oETJwETARMJLapyb',
+	// dong bai
+	1101163: 'mgBSsKWRgsAcAsRosaWRacAcAsBlmyo5IFwBwCwUl7Olxse0o8DEwBwMTAXAxMF8DEtaAEycAEwATABLapyb',
 };
